cfd8557f9b288077046454b6efccffde4acc445e
lanmower <almagestfraternite@gmail.com>
2025-03-31 23:34:55 +0200

    Update index.ts

--- functions/hello-world/index.ts
+++ functions/hello-world/index.ts
@@ -7,8 +7,13 @@ interface reqPayload {
 console.log('hello-world started');
 
 serve(async (req: Request) => {
+  const { name } : reqPayload = await req.json();
+  const data = {
+    message: `Hello ${name} from Supabase Edge Functions!`,
+  }
+
   return new Response(
-        JSON.stringify({ msg: 'hi'}),
-        { status: 200, headers: { "Content-Type": "application/json" } },
-      )
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json", "Connection": "keep-alive" } },
+  )
 }, { port: 9005 })
